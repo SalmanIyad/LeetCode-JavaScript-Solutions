104a9a02f8889140b454294ea27ce48fa9cc4c58
SalmanIyad <110406908+SalmanIyad@users.noreply.github.com>
2024-10-25 01:06:09 -0700

    Time: 492 ms (64.55%), Space: 83.3 MB (20.15%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -8,8 +8,8 @@ var maxSlidingWindow = function(nums, k) {
     let deque = [];
 
     for (let i = 0; i < nums.length; i++) {
-        if (deque.length && deque[0] < i - k + 1) deque.shift();
-        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) deque.pop();
+        if (deque[0] < i - k + 1) deque.shift();
+        while (nums[deque[deque.length - 1]] < nums[i]) deque.pop();
         deque.push(i);
         if (i >= k - 1) maxsArr.push(nums[deque[0]]);
     }
